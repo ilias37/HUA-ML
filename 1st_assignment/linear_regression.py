abed3a4fb4fa1670c1c920206f2fb68f697bd489
iliasp <iliaspanag37@outlook.com>
2023-11-15 02:27:38 +0200

    to montelo mono

---
+++ 1st_assignment/linear_regression.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+class LinearRegression:
+    
+    def __init__(self):
+        self.w = None # Βάρος του μοντέλου
+        self.b = None # Όρος μεροληψίας του μοντέλου
+
+    def fit(self, X, y):
+
+        if not isinstance(X, np.ndarray) or not isinstance(y, np.ndarray):    # Έλεγχος ότι τα X και y είναι numpy arrays 
+            raise ValueError("X and y should be numpy arrays") 
+
+        if X.shape[0] != y.shape[0]:        # 'Ελεγχος αν οι μεταξύ τους διαστάσεις είναι συμβατές               
+            raise ValueError("The number of rows in X and y should be the same")
+
+        ones_column = np.ones((X.shape[0], 1))
+        X = np.concatenate( (X, ones_column), axis=1)
+
+        theta = np.linalg.inv( X.T.dot(X) ).dot(X.T).dot(y)    # Υπολογισμός των παραμέτρων θ
+
+        # Αποθήκευση στις ιδιότητες της κλάσης        
+        self.w = theta[:-1]
+        self.b = theta[-1]
+
+    def predict(self, X):
+
+        if self.w is None or self.b is None:        # Σφάλμα αν το μοντέλο δεν έχει εκπαιδευτεί
+            raise ValueError("Model has not been trained")
+        
+        return np.dot(X, self.w) + self.b             # y^ = Xw + b
+
+    def evaluate(self, X, y):
+
+        if self.w is None or self.b is None:        # Σφάλμα αν το μοντέλο δεν έχει εκπαιδευτεί
+            raise ValueError("Model has not been trained")
+
+        y_hat = self.predict(X)
+        N = X.shape[0]
+        mse = np.dot( (y_hat - y).T, (y_hat - y) ) / N   # MSE = 1/N (y^ − y)T (y^ − y) 
+
+        return (y_hat, mse)
