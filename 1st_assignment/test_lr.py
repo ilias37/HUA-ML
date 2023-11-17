20a6c8ccb9efa0639da7141d0005af5e0dbf00fa
iliasp <iliaspanag37@outlook.com>
2023-11-17 18:08:48 +0200

    oloklhrwmeno 3.2, 3.3

--- 1st_assignment/test_lr.py
+++ 1st_assignment/test_lr.py
@@ -1,6 +1,6 @@
 from sklearn import datasets
 from sklearn import model_selection
-import linear_regression
+import linear_regression    # Το linear_regression.py
 import numpy as np
 from sklearn import linear_model
 from sklearn import metrics
@@ -14,13 +14,10 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(dataset.data
 lr = linear_regression.LinearRegression()        # Δημιουργία μοντέλου
 lr.fit(X_train, y_train)
 
-y_hat_train, mse_train = lr.evaluate(X_train, y_train) 
-rmse_train = np.sqrt(mse_train)
-print("Root Mean Squared Error (RMSE) on training set: {:.10f}".format(rmse_train) )
-
 y_hat_test, mse_test = lr.evaluate(X_test, y_test)
 rmse_test = np.sqrt(mse_test)
-print("Root Mean Squared Error (RMSE) on test set: {:.10f}".format(rmse_test) )
+print("3.1")
+print("RMSE on test set: {}".format(rmse_test) )
 print("")
 
 # 3.2
@@ -28,7 +25,7 @@ rmse_values = []
 
 for _ in range(20):
 
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(dataset.data, dataset.target, test_size=0.3, random_state=42)
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(dataset.data, dataset.target, test_size=0.3)
     
     lr = linear_regression.LinearRegression() 
     lr.fit(X_train, y_train)
@@ -41,6 +38,7 @@ for _ in range(20):
 rmse_values = np.array(rmse_values)
 
 mean_rmse = np.mean(rmse_values)    # Υπολογισμός μέσου του RMSE
+print("3.2")
 print("Mean RMSE over {} iterations: {}".format(20, mean_rmse))
 
 std_rmse = np.std(rmse_values)      # Υπολογισμός τυπικής απόκλισης του RMSE
@@ -52,7 +50,7 @@ rmse_values = []
 
 for _ in range(20):
 
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(dataset.data, dataset.target, test_size=0.3, random_state=42)
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(dataset.data, dataset.target, test_size=0.3)
     
     lr = linear_model.LinearRegression()
     lr.fit(X_train, y_train)
@@ -62,9 +60,10 @@ for _ in range(20):
     rmse = np.sqrt(metrics.mean_squared_error(y_test, y_hat)) # Υπολογισμός του RMSE
     rmse_values.append(rmse)
 
+rmse_values = np.array(rmse_values)
 
 mean_rmse = np.mean(rmse_values)
-print("Mean RMSE: {}".format(mean_rmse))
+print("Mean of RMSE: {}".format(mean_rmse))
 
 std_rmse = np.std(rmse_values)
-print("RMSE Variance: {}".format(std_rmse))
+print("Standard Deviation of RMSE: {}".format(std_rmse))
